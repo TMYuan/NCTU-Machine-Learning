e5678789538fe4341cf61cce4e5db8292bea86e0
TMYuan <piews482zt@gmail.com>
2018-03-23 16:19:11 +0800

    finish task2.

--- homework2/task2_online_learning/main.py
+++ homework2/task2_online_learning/main.py
@@ -1,4 +1,5 @@
 import argparse
+import data
 import numpy as np
 
 parser = argparse.ArgumentParser(description='It is a program for ML HW#1.')
@@ -8,5 +9,21 @@ parser.add_argument('beta_b', help='value "b" of initial beta function', type=in
 args = parser.parse_args()
 
 print('file_path: {}'.format(args.file_path))
-print('beta_a: {}'.format(args.base))
-print('beta_b: {}'.format(args.rate))
+print('beta_a: {}'.format(args.beta_a))
+print('beta_b: {}'.format(args.beta_b))
+
+# read data from the path
+x = data.read(args.file_path)
+
+# for each element of x, calculate the count of 0 and the count of 1
+beta_a = args.beta_a
+beta_b = args.beta_b
+for batch in x:
+    count_zero = batch.count(0)
+    count_one = batch.count(1)
+    print('-'*20)
+    print('Prior: Beta({}, {})'.format(beta_a, beta_b))
+    print('Binmail Likelihood: {}'.format((beta_a+count_one)/(beta_a+count_one+beta_b+count_zero)))
+    beta_a = beta_a + count_one
+    beta_b = beta_b + count_zero
+    print('Posterior: Beta({}, {})'.format(beta_a, beta_b))
